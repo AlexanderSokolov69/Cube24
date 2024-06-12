2e373d30c5d96d1b1e2c61b467157bc2684d1c7c
AlexanderSokolov69 <sokoloval@yandexlyceum.ru>
2024-06-12 20:50:42 +0300

    Login and MDI panel is ready

--- misc/functions.py
+++ misc/functions.py
@@ -3,9 +3,21 @@
 import sys
 import traceback as tb
 
+from PyQt5.QtWidgets import QMessageBox
+
+from misc.logwriter import flog
+
 
 def except_hook(cls, exception, traceback):
-    global flog
     flog.to_log(f"""{exception} | \n{tb.format_tb(traceback)[0]}""")
     sys.__excepthook__(cls, exception, traceback)
+
+
+def msg_window(title='Сообщение', text='Сообщение о событии!', count=1):
+    box = QMessageBox()
+    box.setWindowTitle(title)
+    box.setText(text)
+    box.setStandardButtons(QMessageBox.Ok)
+    box.exec_()
+
 
