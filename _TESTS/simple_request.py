982be37f140190aead583aeb98f1fad693a9a4b7
AlexanderSokolov69 <sokoloval@yandexlyceum.ru>
2024-06-17 10:07:26 +0300

    Login and MDI panel is ready

--- _TESTS/simple_request.py
+++ _TESTS/simple_request.py
@@ -6,12 +6,12 @@ from requests import get, post
 # pprint(get('http://localhost:5050/api/v2/user/15').json())
 
 data = {'login': 'sa', 'password': '030669'}
-response = post('http://localhost:5050/api/login', json=data).json()
+response = post('http://85.93.48.241:5077/api/login', json=data).json()
 print(response['message'])
 token = response.get('token', None)
 pprint(token)
 
 data = {'idUsers': 512}
 headers = {'Authorization': 'Bearer ' + token}
-response = get('http://localhost:5050/api/secret', headers=headers, json=data).json()
+response = get('http://85.93.48.241:5077/api/secret', headers=headers, json=data).json()
 print(response)
